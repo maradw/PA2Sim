a893d5776dec322caf813d9bafe8c77fed521cd0
maradw <maria.mendoza@tecsup.edu.pe>
2024-05-05 12:38:48 -0500

    End

---
+++ Assets/MuteAudio.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteAudio : MonoBehaviour
+{
+    [SerializeField] private Camera m_camera;
+    bool enable = true;
+    private void Start()
+    {
+        //m_camera = m_camera.GetComponent<AudioListener>();
+    }
+    private void Update()
+    {
+        //if(enable )
+    }
+}
